3462997a1ca76f6fa65ea19e58d120e2e296f8db
gnanee57 <gnanee57@gmail.com>
2020-07-09 21:22:07 +0530

    Assignment-2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -12,15 +12,10 @@ export class MenuComponent implements OnInit {
 
   dishes: Dish[];
 
-  selectedDish: Dish;
-
-  constructor(private dishService: DishService) { }
+  constructor(private dishservice: DishService) { }
 
   ngOnInit(): void {
-    this.dishes = this.dishService.getDishes();
+    this.dishes = this.dishservice.getDishes();
   }
 
-  onSelect(dish:Dish){
-    this.selectedDish = dish;
-  }
 }
